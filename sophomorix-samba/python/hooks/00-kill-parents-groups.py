1ab84ba860b28743243047e2cf156b305ee11612
kiarn <arnaud@linuxmuster.net>
2025-04-07 17:29:35 +0200

    Hook script for student kill.

---
+++ sophomorix-samba/python/hooks/00-kill-parents-groups.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+# This script is part of the sophomorix project and intended to delete
+# obsolete CN of parents in students OU
+# DO NOT EDIT OR REMOVE IT !
+
+import sys
+from linuxmusterTools.common import *
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr, LMNUserWriter
+
+
+epoch = sys.argv[1]
+school = sys.argv[2]
+
+uw = LMNUserWriter()
+
+if epoch is None:
+    updates = parse_update_log(today=True)
+    timestamps = list(updates.keys())
+    timestamps.sort()
+    epoch = timestamps[-1]
+    entries = updates[epoch]
+else:
+    entries = parse_add_log(epoch=epoch)
+
+for entry in entries:
+    user = entry["user"]
+    role = entry["role"]
+    lprint.info(f"Checkng user {user}")
+    if role == 'student':
+        uw.del_parent_group(user)
+
+
